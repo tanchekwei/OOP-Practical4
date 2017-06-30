69365f90fff8001a74224cc79a6d31f9b38873d2
coolwei1 <weiwei1@live.com.my>
2017-06-30 10:53:02 +0800

    now commit

--- src/P4Q2/P4Q2.java
+++ src/P4Q2/P4Q2.java
@@ -7,12 +7,38 @@ package P4Q2;
 
 /**
  *
- * @author Chek Wei
+ * @author Tan Chek Wei
  */
 public class P4Q2 {
     public static void main(String[] args) {
+        
+        //Student 1
         Student student1 = new Student("16WMD02116", "Tan Chek Wei");
-        student1.setNumberofQuiz(40);
-        student1.setTotalQuizScore(35);
+        Student student2 = new Student();
+        student1.addQuiz(8);
+        student1.addQuiz(9);
+        student1.addQuiz(6);
+        
+        //Student 2
+        student2.setStudentID("16WMD03759");
+        student2.setName("Tan Han Zong");
+        student2.addQuiz(10);
+        student2.addQuiz(9);
+        student2.addQuiz(10);
+        
+        System.out.println("\nStudent 1:");
+        System.out.println("----------");
+        System.out.println("Student ID  : " + student1.getStudentID());
+        System.out.println("Student name: " + student1.getName());
+        System.out.println("Total score : " + student1.getTotalQuizScore());
+        System.out.println("Average     : " + student1.getAverageScore());
+        
+        
+        System.out.println("\nStudent 2:");
+        System.out.println("----------");
+        System.out.println("Student ID  : " + student2.getStudentID());
+        System.out.println("Student name: " + student2.getName());
+        System.out.println("Total score : " + student2.getTotalQuizScore());
+        System.out.println("Average     : " + student2.getAverageScore());
     }
 }
